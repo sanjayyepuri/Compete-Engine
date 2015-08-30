943536cdb23ef05d6f4b8f126cda9b4e4327545c
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2015-08-29 23:34:12 -0500

    Compete-Web module added
    Created a Web App separate from the REST API. Enabled CORS on the REST
    Api.

--- Compete-Server/server.js
+++ Compete-Server/server.js
@@ -36,6 +36,12 @@ app.use(cookieParser());
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Routes
 //require('./app/routes/routes.js')(app, passport); TODO add passport
 require('./app/routes/routes.js')(app);
